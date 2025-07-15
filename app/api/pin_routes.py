3b6e2441996bda01a5b22343f2686f0ffdd7c054
laquisha-ayee <rayelaquisha@gmail.com>
2025-07-15 13:49:15 -0700

    notes added

--- app/api/pin_routes.py
+++ app/api/pin_routes.py
@@ -4,11 +4,13 @@ from app.models import Pin, db
 
 pin_routes = Blueprint('pins', __name__)
 
+#get all pins &return list of pins
 @pin_routes.route('/', methods=['GET'])
 def get_all_pins():
     pins = Pin.query.all()
     return jsonify([pin.to_dict() for pin in pins])
 
+#get a single pin by ID
 @pin_routes.route('/<int:id>', methods=['GET'])
 def get_pin_by_id(id):
     pin = Pin.query.get(id)
@@ -16,6 +18,7 @@ def get_pin_by_id(id):
         return pin.to_dict()
     return {'errors': 'Pin not found'}, 404
 
+#only logged in users can create a new pin
 @pin_routes.route('/', methods=['POST'])
 @login_required
 def create_pin():
@@ -31,6 +34,7 @@ def create_pin():
     db.session.commit()
     return new_pin.to_dict(), 201
 
+#only owner can update pin
 @pin_routes.route('/<int:id>', methods=['PUT'])
 @login_required
 def update_pin(id):
@@ -40,6 +44,7 @@ def update_pin(id):
     if pin.user_id != current_user.id:
         return {'errors': 'Unauthorized'}, 403
 
+#update pin if new info is added
     data = request.get_json()
     pin.title = data.get('title', pin.title)
     pin.image_url = data.get('image_url', pin.image_url)
@@ -48,6 +53,7 @@ def update_pin(id):
     db.session.commit()
     return pin.to_dict()
 
+#only owner can delete pins
 @pin_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 def delete_pin(id):
@@ -60,3 +66,4 @@ def delete_pin(id):
     db.session.delete(pin)
     db.session.commit()
     return {'message': 'Pin deleted successfully'}
+    #deleted pin confermation 
