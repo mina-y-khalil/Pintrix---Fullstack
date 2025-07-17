53c7bffc6dc73ac736e33b825d1a112a70bb84fe
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-07-16 17:54:43 -0700

    Add comments Redux slice and API thunks

--- react-vite/src/redux/store.js
+++ react-vite/src/redux/store.js
@@ -6,9 +6,11 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
+import commentsReducer from "./comments";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  comments: commentsReducer,
 });
 
 let enhancer;
