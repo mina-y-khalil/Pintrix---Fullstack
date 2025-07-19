6c15c1ecbaf37ed9a19cba1f4b8016d10855b516
Thomas Yemane <tomblue246@gmail.com>
2025-07-19 13:00:43 -0700

    Add favorites link to Navigation

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,27 +1,17 @@
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 function Navigation() {
-  const user = useSelector((state) => state.session.user);
-
   return (
     <ul>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
 
-      {user && (
-        <>
-          <li>
-            <NavLink to="/favorites">❤️ Favorites</NavLink>
-          </li>
-          <li>
-            <ProfileButton />
-          </li>
-        </>
-      )}
+      <li>
+        <ProfileButton />
+      </li>
     </ul>
   );
 }
