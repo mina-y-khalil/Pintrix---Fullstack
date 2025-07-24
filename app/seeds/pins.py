29497070e0a0aa109b8c46d8c82c848c8fa5e665
laquisha-ayee <rayelaquisha@gmail.com>
2025-07-23 18:11:19 -0700

    new pins seeded

--- app/seeds/pins.py
+++ app/seeds/pins.py
@@ -2,33 +2,73 @@ from app.models import db, Pin, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_pins():
-    pin1 = Pin(
-        user_id=1,
-        image_url='https://images.pexels.com/photos/1395305/pexels-photo-1395305.jpeg',
-        title='Antique Pearl Ring',
-        description='Diamonds and pearls together create a soft and enchanting aesthetic.',
-        likes_count=3  # matches number of favorites for pin 1
-    )
-    pin2 = Pin(
-        user_id=2,
-        image_url='https://images.pexels.com/photos/2265876/pexels-photo-2265876.jpeg',
-        title='Floating Along',
-        description='Boat gathering on the beautiful coast of Peru.',
-        likes_count=1  # matches number of favorites for pin 2
-    )
-    pin3 = Pin(
-        user_id=3,
-        image_url='https://images.pexels.com/photos/59989/pexels-photo-59989.jpeg',
-        title='Elephant Family',
-        description='This pack of elephants are hanging out around watering hole, enjoying the sun',
-        likes_count=1  # matches number of favorites for pin 3
-    )
+    pins = [
+        # Jewelry board
+        Pin(user_id=1, image_url='https://images.pexels.com/photos/1395305/pexels-photo-1395305.jpeg',
+            title='Antique Pearl Ring',
+            description='Diamonds and pearls together create a soft and enchanting aesthetic.',
+            likes_count=3),
+        
+        Pin(user_id=2, image_url='https://images.pexels.com/photos/6625939/pexels-photo-6625939.jpeg',
+            title='Say yess!',
+            description='Beautiful ring set for your special day',
+            likes_count=1),
+        
+        Pin(user_id=3, image_url='https://images.pexels.com/photos/6625937/pexels-photo-6625937.jpeg',
+            title='Peacock love',
+            description='Make a statement with this beautiful necklace! Inspired by the beautiful peacock.',
+            likes_count=1),
+        
+        Pin(user_id=1, image_url='https://images.pexels.com/photos/3634366/pexels-photo-3634366.jpeg',
+            title='Timeless bracelet',
+            description='This bracelet is stunning and goes with everything, it can be worn every day!',
+            likes_count=1),
 
-    # Add pins to the session and commit
-    db.session.add_all([pin1, pin2, pin3])
+        # Vacation board
+        Pin(user_id=2, image_url='https://images.pexels.com/photos/2265876/pexels-photo-2265876.jpeg',
+            title='Floating Along',
+            description='Boat gathering on the beautiful coast of Peru.',
+            likes_count=1),
+        
+        Pin(user_id=1, image_url='https://images.pexels.com/photos/33111360/pexels-photo-33111360.jpeg',
+            title='Sail away!',
+            description='These boat rentals are great for using while on vacation',
+            likes_count=1),
+        
+        Pin(user_id=2, image_url='https://images.pexels.com/photos/33068153/pexels-photo-33068153.png',
+            title='Cruisin easy',
+            description='Cruise ships are always a good time. One flat rate for an all-inclusive vacation',
+            likes_count=1),
+        
+        Pin(user_id=3, image_url='https://images.pexels.com/photos/33052728/pexels-photo-33052728.jpeg',
+            title='Family fun',
+            description='So many fun activities for the kids! A great place to make wonderful memories',
+            likes_count=1),
+
+        # Animal board
+        Pin(user_id=2, image_url='https://images.pexels.com/photos/59989/pexels-photo-59989.jpeg',
+            title='Elephant Family',
+            description='This pack of elephants are hanging out around a watering hole, enjoying the sun',
+            likes_count=1),
+        
+        Pin(user_id=3, image_url='https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg',
+            title='Kitten love!',
+            description='These adorable four-week-old kittens enjoying the grass for the first time.',
+            likes_count=1),
+        
+        Pin(user_id=1, image_url='https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg',
+            title='Double the trouble',
+            description='Father and son looking like twins',
+            likes_count=1),
+        
+        Pin(user_id=2, image_url='https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg',
+            title='Feeling sheepish',
+            description='He’s a little confused after being asked to say cheese',
+            likes_count=1)
+    ]
+
+    db.session.add_all(pins)
     db.session.commit()
-
-
 
 def undo_pins():
     if environment == "production":
@@ -36,4 +76,3 @@ def undo_pins():
     else:
         db.session.execute(text("DELETE FROM pins"))
     db.session.commit()
-
