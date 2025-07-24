39c40cd6da51fefeafc229f2b8d36eba4741f90c
laquisha-ayee <rayelaquisha@gmail.com>
2025-07-23 19:04:42 -0700

    pins css started

--- react-vite/src/components/Pins/PinsGrid.jsx
+++ react-vite/src/components/Pins/PinsGrid.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchPins } from "../../redux/pins";
 import { createFavorite, deleteFavorite } from "../../redux/favorites";
+import "./PinsGrid.css";
 
 export default function PinsGrid() {
   const dispatch = useDispatch();
   const pins = useSelector(state => Object.values(state.pins));
   const favorites = useSelector(state => state.favorites || {});
   const currentUser = useSelector(state => state.session.user);
+  const [expandedPins, setExpandedPins] = useState(new Set());
 
   useEffect(() => {
     dispatch(fetchPins());
@@ -22,33 +24,57 @@ export default function PinsGrid() {
     }
   };
 
+  const handlePinClick = (pinId) => {
+    setExpandedPins(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(pinId)) {
+        newSet.delete(pinId);
+      } else {
+        newSet.add(pinId);
+      }
+      return newSet;
+    });
+  };
+
   return (
     <div className="pins-grid">
       {pins.length ? (
         pins.map(pin => (
           <div key={pin.id} className="pin-card">
-            <Link to={`/pins/${pin.id}`}>
-              <h3>{pin.title}</h3>
-              <img src={pin.imageUrl} alt={pin.title} />
-            </Link>
-
-            <p>{pin.description}</p>
-
-            {/* Favorite Toggle */}
-            <button onClick={() => handleFavoriteToggle(pin.id)}>
-              {favorites[pin.id] ? "💔 Unfavorite" : "💖 Favorite"}
-            </button>
+            <div className="pin-header">
+              <Link to={`/pins/${pin.id}`}>
+                <h3>{pin.title}</h3>
+              </Link>
+              <button 
+                className={`favorite-button ${favorites[pin.id] ? 'favorited' : ''}`}
+                onClick={() => handleFavoriteToggle(pin.id)}
+              >
+                ♥
+              </button>
+            </div>
+            
+            <div className="pin-content" onClick={() => handlePinClick(pin.id)}>
+              <img src={pin.image_url} alt={pin.title} /> 
+              
+              {expandedPins.has(pin.id) && (
+                <div className="pin-description">
+                  <p>{pin.description}</p>
+                </div>
+              )}
+            </div>
 
             {/* Conditional Edit Button */}
             {currentUser?.id === pin.user_id && (
-              <Link to={`/pins/${pin.id}/edit`}>
-                <button>Edit</button>
-              </Link>
+              <div className="edit-link-container">
+                <Link to={`/pins/${pin.id}/edit`} className="edit-link">
+                  Edit
+                </Link>
+              </div>
             )}
           </div>
         ))
       ) : (
-        <p>No pins yet. Let’s get creative!</p>
+        <p>Pins Loading ...</p>
       )}
     </div>
   );
